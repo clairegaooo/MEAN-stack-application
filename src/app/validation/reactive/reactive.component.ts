0d373bb348874c1f16c917409253ca852f988e02
clairegaooo <50030603+clairegaooo@users.noreply.github.com>
2019-04-27 21:24:34 -0400

    Update reactive.component.ts

--- src/app/validation/reactive/reactive.component.ts
+++ src/app/validation/reactive/reactive.component.ts
@@ -10,10 +10,7 @@ import { Router } from '@angular/router';
 })
 export class ReactiveComponent implements OnInit {
   userForm: FormGroup;
-  username: string;
-  password: string;
-  email: string;
-  gender: string;
+  
   constructor(private _fb: FormBuilder, private _http: HttpClient, private _router: Router) {
     this.userForm = this._fb.group({
       username: ['amber', [Validators.required, Validators.minLength(3)]],
@@ -27,13 +24,7 @@ export class ReactiveComponent implements OnInit {
   }
 
   saveUser() {
-    const newUser = {
-      username: this.username,
-      password: this.password,
-      email: this.email,
-      gender: this.gender
-    };
-    this._http.post('http://localhost:2210/saveusers', newUser).subscribe((data: any) => {
+    this._http.post('http://localhost:2210/saveusers', this.userForm.value).subscribe((data: any) => {
       if (data.success) {
         this._router.navigate(['/login']);
       } else {
